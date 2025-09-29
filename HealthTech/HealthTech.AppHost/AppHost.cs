7028b4805daa4b51d3fa1fdfa6d8ddd16c17b5fa
Guilherme Coutinho <coutinho.gui@gmail.com>
2025-09-29 01:16:07 +0000

    funcionando local

--- HealthTech/HealthTech.AppHost/AppHost.cs
+++ HealthTech/HealthTech.AppHost/AppHost.cs
@@ -1,11 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.HealthTech_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health");
-
-var api = builder.AddProject<Projects.HealthTech_ApiService>("apiservice")
+    .WithHttpHealthCheck("/health")
     .WithEnvironment("Supabase__Url",   Environment.GetEnvironmentVariable("Supabase__Url") ?? "")
     .WithEnvironment("Supabase__ServiceKey", Environment.GetEnvironmentVariable("Supabase__ServiceKey") ?? "");
+
 
 builder.AddProject<Projects.HealthTech_Web>("webfrontend")
     .WithExternalHttpEndpoints()
